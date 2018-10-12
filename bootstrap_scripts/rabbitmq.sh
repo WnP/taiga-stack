8b540e68035140197cbb785c45dbfe01606747f5
Steeve Chailloux <steevechailloux@gmail.com>
2018-10-12 17:58:05 +0200

    add rabbitmq celery vhost

--- bootstrap_scripts/rabbitmq.sh
+++ bootstrap_scripts/rabbitmq.sh
@@ -3,3 +3,6 @@
 rabbitmqctl add_user taiga secret
 rabbitmqctl add_vhost taiga
 rabbitmqctl set_permissions -p taiga taiga ".*" ".*" ".*"
+# Celery
+rabbitmqctl add_vhost celery
+rabbitmqctl set_permissions -p celery taiga ".*" ".*" ".*"
