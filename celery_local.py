0abbcb309b326dc397dfe7557735454db67304b6
Steeve Chailloux <steevechailloux@gmail.com>
2018-10-12 18:01:45 +0200

    fix rabbitmq vhost should not contain ending slash

--- celery_local.py
+++ celery_local.py
@@ -2,5 +2,5 @@ from .celery import *  # noqa
 
 # To use celery in memory
 # task_always_eager = True
-broker_url = "amqp://taiga:secret@tasks.rabbitmq:5672/celery/"
+broker_url = "amqp://taiga:secret@tasks.rabbitmq:5672/celery"
 result_backend = 'redis://tasks.redis:6379/0'
