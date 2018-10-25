d30ad9600ab4885440390405b51dbda35093d118
Steeve Chailloux <steevechailloux@gmail.com>
2018-10-25 13:47:00 +0200

    [fix] celery config

--- back/celery_local.py
+++ back/celery_local.py
@@ -4,3 +4,6 @@ from .celery import *  # noqa
 # task_always_eager = True
 broker_url = "amqp://taiga:secret@tasks.rabbitmq:5672/celery"
 result_backend = 'redis://tasks.redis:6379/0'
+
+with open('/run/secrets/taiga-back-celery-settings-orus-io') as django_secrets:
+    exec(django_secrets.read())
